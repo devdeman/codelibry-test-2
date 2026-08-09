dad203f6006d0d394d1217709077599488e6e25c
Dmytro Piankovskiy <d.piankovskyi@devsx.net>
2026-08-09 22:30:35 +0300

    Initial commit

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1784724527;s:7:"authKey";s:64:".Weu)bk<%lf+xl%U6kzG/%V7FC|08mOWZEo[hO;K1+n/&4]mX{(239.oPKp]$K80";s:7:"version";s:5:"1.1.0";}
